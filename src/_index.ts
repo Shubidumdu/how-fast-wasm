3901d1416894baeb9422b5f93ca7b5ff0cdf7145
Alan Seo <alan@grepp.co>
2022-04-17 02:59:00 +0900

    lint for _index.ts

--- src/_index.ts
+++ src/_index.ts
@@ -1,4 +1,4 @@
-import { greet, dfs as rustDfs } from "my-wasm";
+import { greet, dfs as rustDfs } from 'my-wasm';
 import '../style/index.scss';
 
 const visited: boolean[][] = [...new Array(6)].map(() => Array(6).fill(false));
@@ -11,12 +11,12 @@ const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
 
 const dfs = () => {
   const stacks = [start];
-  
+
   while (stacks.length) {
     const [row, col] = stacks.pop();
     const [targetRow, targetCol] = target;
     if (row === targetRow && col === targetCol) {
-      console.log('JS target found!: ', `(${row}, ${col})`); 
+      console.log('JS target found!: ', `(${row}, ${col})`);
       break;
     }
     console.log(row, col);
